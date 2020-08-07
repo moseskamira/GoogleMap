f4572a51ce6673119b6eefd754a2df1f1bb4eae7
moseskamira <moses.african@gmail.com>
2020-08-07 23:12:57 +0300

    map view

---
+++ app/src/main/java/com/example/googlemap/Constants.java
@@ -0,0 +1,9 @@
+package com.example.googlemap;
+
+import android.content.res.Resources;
+
+
+class Constants {
+    static Resources resources;
+
+}
--- app/src/main/java/com/example/googlemap/MainActivity.java
+++ app/src/main/java/com/example/googlemap/MainActivity.java
@@ -10,24 +10,29 @@ import android.os.Bundle;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private static final int LOCATION_REQUEST_CODE = 10;
+    MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SupportMapFragment supportMapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map_fragment);
-        if (supportMapFragment != null) {
-            supportMapFragment.getMapAsync(this);
+        mapView = findViewById(R.id.map_fragment);
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            String API_KEY = Constants.resources.getString(R.string.goole_api_key);
+            mapViewBundle = savedInstanceState.getBundle(API_KEY);
         }
+        mapView.onCreate(mapViewBundle);
+        mapView.getMapAsync(this);
+
     }
 
 
@@ -35,10 +40,11 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onMapReady(GoogleMap googleMap) {
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager
+                    .PERMISSION_GRANTED) {
                 requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
                         LOCATION_REQUEST_CODE);
-            }else {
+            } else {
                 displayLocation(googleMap);
             }
         }
@@ -46,21 +52,54 @@ public class MainActivity extends AppCompatActivity implements OnMapReadyCallbac
     }
 
     private void displayLocation(GoogleMap map) {
+
         LatLng kampala = new LatLng(0.337, 32.57);
         map.addMarker(new MarkerOptions()
                 .position(kampala))
                 .setTitle("Marker");
         map.moveCamera(CameraUpdateFactory.newLatLng(kampala));
-
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
         switch (requestCode) {
             case LOCATION_REQUEST_CODE:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
 
                 }
         }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
     }
 }
