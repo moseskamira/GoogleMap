7d009817ae0e04484f02d73ea07f0970441086f9
moseskamira <moses.african@gmail.com>
2020-08-07 09:48:52 +0300

    Google Map

---
+++ app/src/main/java/com/example/googlemap/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.googlemap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+    MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        Bundle mapViewBundle = null;
+        if (savedInstanceState != null) {
+            mapViewBundle = savedInstanceState.getBundle("MapViewBundleKey");
+
+        }
+        mapView = findViewById(R.id.map_view_id);
+        mapView.onCreate(mapViewBundle);
+        mapView.getMapAsync(this);
+
+
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(0,0))).setTitle("Marker");
+
+    }
+}
